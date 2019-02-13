a0fb48d30e5f156f477f9b7a2b153fb4f780d627
WebMapaX <47189938+WebMapaX@users.noreply.github.com>
2019-02-13 15:57:41 -0200

    Add files via upload

---
+++ data/HyBrasil_6.js
@@ -0,0 +1,8 @@
+var json_HyBrasil_6 = {
+"type": "FeatureCollection",
+"name": "HyBrasil_6",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "nome": "HyBrasil" }, "geometry": { "type": "Point", "coordinates": [ -47.641902332717571, -14.174890823818906 ] } }
+]
+}
---
+++ data/Quadrante_2.js
@@ -0,0 +1,12 @@
+var json_Quadrante_2 = {
+"type": "FeatureCollection",
+"name": "Quadrante_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "REF": null }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ -48.537551652588462, -13.094641557864595 ], [ -48.563787027757733, -14.738719142581537 ] ] ] } },
+{ "type": "Feature", "properties": { "REF": null }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ -48.563681738610946, -14.739550982047838 ], [ -46.782254354741369, -14.762489080870534 ] ] ] } },
+{ "type": "Feature", "properties": { "REF": null }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ -46.781946970067452, -14.76279297261928 ], [ -46.769192036664109, -13.093250054813465 ] ] ] } },
+{ "type": "Feature", "properties": { "REF": null }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ -46.770924227197682, -13.099048735328463 ], [ -48.132539924285346, -13.096740902417816 ], [ -48.539979918686711, -13.098859055968816 ] ] ] } },
+{ "type": "Feature", "properties": { "REF": "Mapa de localização na Chapada dos Veadeiros" }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ -47.465675773949329, -13.096231721029174 ], [ -46.870245292030489, -13.09936410558934 ] ] ] } }
+]
+}
